882fce4f3370aa007f4b08c0624404d57ba08b79
andrawhoodoo <43014649+andrawhoodoo@users.noreply.github.com>
2018-11-25 19:42:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 //set the template engine ejs
 app.set('view engine', 'ejs')
@@ -27,18 +28,42 @@ const io = require("socket.io")(server)
 io.on('connection', (socket) => {
 	console.log('New user connected')
 
+
 	//default username
 	socket.username = "Anonymous"
 
     //listen on change_username
     socket.on('change_username', (data) => {
         socket.username = data.username
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.collection("chatlog").find({}).toArray(function(err, result) {
+                if (err) throw err;
+                for(i = 0; i < result.length; i = i +1) {
+                    if(result[i].message !== "")
+                    socket.emit('new_message', {message : result[i].message, username : result[i].name});
+                }
+                db.close();
+            });
+        });
+        io.sockets.emit('new_message', {message : "has joined the conversation.", username : socket.username})
     })
 
     //listen on new_message
     socket.on('new_message', (data) => {
         //broadcast the new message
         io.sockets.emit('new_message', {message : data.message, username : socket.username});
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            var myobj = { name: socket.username, message: data.message };
+            dbo.collection("chatlog").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            });
+        });
     })
 
     //listen on typing
