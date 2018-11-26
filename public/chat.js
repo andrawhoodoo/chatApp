e0e428842be4564c45c64b884674b10afbb7a430
andrawhoodoo <43014649+andrawhoodoo@users.noreply.github.com>
2018-11-25 21:14:21 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,68 +1,47 @@
-const express = require('express')
-const app = express()
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
 
-//set the template engine ejs
-app.set('view engine', 'ejs')
+$(function(){
+   	//make connection
+	var socket = io.connect('http://localhost:3000')
 
-//middlewares
-app.use(express.static('public'))
-
-
-//routes
-app.get('/', (req, res) => {
-	res.render('index')
-})
-
-//Listen on port 3000
-server = app.listen(3000)
+	//buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+	var feedback = $("#feedback")
 
 
+	//Emit message
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
 
-//socket.io instantiation
-const io = require("socket.io")(server)
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		feedback.html('');
+		message.val('');
+		chatroom.append("<p class='message'>" + data.username + ": 			" + data.message + "</p>")
+	})
+
+	//Emit a username
+	send_username.click(function(){
+		if(username.val() !== "") {
+			socket.emit('change_username', {username : username.val()})
+			document.getElementById("change_username").remove()
+		}
+		else window.alert("User Name must be non-empty.");
+	})
+
+	//Emit typing
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	})
+
+	//Listen on typing
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	})
+});
 
 
-//listen on every connection
-io.on('connection', (socket) => {
-	console.log('New user connected')
-
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("mydb");
-            dbo.collection("chatlog").find({}).toArray(function(err, result) {
-                if (err) throw err;
-                for(i = 0; i < result.length; i = i +1) {
-                    if(result[i].message !== "")
-                    socket.emit('new_message', {message : result[i].message, username : result[i].name});
-                }
-                db.close();
-            });
-        });
-    })
-
-    //listen on new_message
-    socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("mydb");
-            var myobj = { name: socket.username, message: data.message };
-            dbo.collection("chatlog").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-            });
-        });
-    })
-
-    //listen on typing
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
-})
