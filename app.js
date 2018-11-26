440666ec1c352b8cabe99aa66af9a2f42fdf7384
andrawhoodoo <adacunto@lions.molloy.edu>
2018-11-26 18:18:34 -0500

    final updates

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ server = app.listen(3000)
 //socket.io instantiation
 const io = require("socket.io")(server)
 
+
 //listen on every connection
 io.on('connection', (socket) => {
 	console.log('New user connected')
